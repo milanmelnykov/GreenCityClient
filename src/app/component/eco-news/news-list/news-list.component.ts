0bbf0e8ccc0b4e73aca245fe14fa526752a75a21
KimAT25 <kimat60@gmail.com>
2020-05-08 17:13:33 +0300

    added Masonry lay-out, need to fix async waiting

--- src/app/component/eco-news/news-list/news-list.component.ts
+++ src/app/component/eco-news/news-list/news-list.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import {Component, OnInit, Input, AfterViewInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { EcoNewsService } from 'src/app/service/eco-news/eco-news.service';
 import { Subscription } from 'rxjs';
 import { EcoNewsModel } from '../../../model/eco-news/eco-news-model';
+
+declare let Masonry: any;
 
 @Component({
   selector: 'app-news-list',
   templateUrl: './news-list.component.html',
   styleUrls: ['./news-list.component.css']
 })
-export class NewsListComponent implements OnInit, OnDestroy {
+export class NewsListComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('masonryGrid', {static: true}) masonryGrid: ElementRef;
+
   private view: boolean;
   private iterator: number;
   private gridOutput: Array<string>;
@@ -16,15 +20,70 @@ export class NewsListComponent implements OnInit, OnDestroy {
   private allEcoNews: EcoNewsModel[] = [];
   private elements: EcoNewsModel[] = [];
   public remaining = 0;
+  data = [
+    {
+      creationDate:"2020-04-29T10:57:45.985Z",
+      imagePath:"https://storage.cloud.google.com/staging.greencity-c5a3a.appspot.com/d1a22901-e1e2-4bb3-99d5-98ffe672ee90",
+      id:230,
+      title:"3D-printed corals could improve bioenergy and help coral reefs",
+      text:"Researchers from Cambridge University and University of California San Diego have 3D printed coral-inspired structures that are capable of growing dense populations of microscopic algae",
+      author:{"id":13,"name":"Taras"},
+      tags:["news","ads"]},
+    {
+      creationDate:"2020-04-29T10:57:45.985Z",
+      imagePath:"https://storage.cloud.google.com/staging.greencity-c5a3a.appspot.com/d1a22901-e1e2-4bb3-99d5-98ffe672ee90",
+      id:230,
+      title:"title",
+      text:"Researchers from Cambridge University and University of California San Diego have 3D printed coral-inspired structures that are capable of growing dense populations of microscopic algae",
+      author:{"id":13,"name":"Taras"},
+      tags:["news","ads"]},
+    {
+      creationDate:"2020-04-29T10:57:45.985Z",
+      imagePath:"https://storage.cloud.google.com/staging.greencity-c5a3a.appspot.com/d1a22901-e1e2-4bb3-99d5-98ffe672ee90",
+      id:230,
+      title:"title",
+      text:"text",
+      author:{"id":13,"name":"Taras"},
+      tags:["news","ads"]},
+    {
+      creationDate:"2020-04-29T10:57:45.985Z",
+      imagePath:"https://storage.cloud.google.com/staging.greencity-c5a3a.appspot.com/d1a22901-e1e2-4bb3-99d5-98ffe672ee90",
+      id:230,
+      title:"title",
+      text:"text",
+      author:{"id":13,"name":"Taras"},
+      tags:["news","ads"]},
+    {
+      creationDate:"2020-04-29T10:57:45.985Z",
+      imagePath:"https://storage.cloud.google.com/staging.greencity-c5a3a.appspot.com/d1a22901-e1e2-4bb3-99d5-98ffe672ee90",
+      id:230,
+      title:"title",
+      text:"text",
+      author:{"id":13,"name":"Taras"},
+      tags:["news","ads"]},
+    {
+      creationDate:"2020-04-29T10:57:45.985Z",
+      imagePath:"https://storage.cloud.google.com/staging.greencity-c5a3a.appspot.com/d1a22901-e1e2-4bb3-99d5-98ffe672ee90",
+      id:230,
+      title:"title",
+      text:"text",
+      author:{"id":13,"name":"Taras"},
+      tags:["news","ads"]}
+  ];
 
   constructor(private ecoNewsService: EcoNewsService) { }
 
+  ngAfterViewInit() {
+    this.masonryLayout();
+  }
+
   ngOnInit() {
+    // this.elements = [...this.data];
     this.ecoNewsService.getEcoNewsList();
     this.fetchAllEcoNews();
   }
 
-  private fetchAllEcoNews(): void {
+  private fetchAllEcoNews():void {
     this.ecoNewsSubscription = this.ecoNewsService
       .newsListSubject
       .subscribe(this.setAllAndStartingElems.bind(this));
@@ -61,6 +120,14 @@ export class NewsListComponent implements OnInit, OnDestroy {
   private getFilterData(value: Array<string>): void {
     this.gridOutput = value;
     this.ecoNewsService.getEcoNewsFilteredByTag(value);
+  }
+
+  private masonryLayout() {
+    new Masonry(this.masonryGrid.nativeElement, {
+      itemSelector: '.gallery-view-li-active',
+      columnWidth: 32,
+      horizontalOrder: true
+    });
   }
 
   ngOnDestroy() {
