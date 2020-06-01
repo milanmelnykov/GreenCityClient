519ddcf798a2af1aa55c4d9802ba7e8c310c72fb
Milan Melnykov <39458844+milanmelnykov@users.noreply.github.com>
2020-06-01 22:34:23 +0300

    Delete Chrome headless mode

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -13,11 +13,8 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-  chromeOptions: {
-    args: [ "--headless" ]
-  },
   'browserName': 'chrome'
-},
+  },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
